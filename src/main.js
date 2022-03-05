86a19a4a52dbefa3d62a3c2b91570f5111d5e311
Viviane Soares <vianesoaresss@gmail.com>
2022-03-04 21:20:46 -0300

    ArquivosJSDivididos

--- src/main.js
+++ src/main.js
@@ -1,55 +1,42 @@
-//import { example } from './data.js';
-import data from './data/rickandmorty/rickandmorty.js';
+import data from "./data/rickandmorty/rickandmorty.js";
+import filter from './data.js';
 
-
-
-
-// FUNÇÃO DE FILTRAGEM DE ESPECIE
-
-const filtroEspecie = document.querySelector("#filtro");
-filtroEspecie.addEventListener("change", function (event) { //Função de mudança do seletor
-
-  const resultado = data.results.filter((item) => {  //item é cada 1 dos objetos do Array
-    return item.species === event.target.value; // Condição verdadeira
-  });
-
-  let mostrarCard = ""; //Condição: para cada elemento da array, adicionar um card (DIV com as propriedas)
-  for (let cards of resultado) {  // cards é a variavel [i] usado para iniciar o loop
-    mostrarCard += cards = `<div class="cards">
-    <img src="${cards.image}" class="image-card">
-    <p class="p-name-card">${cards.name}</p>
-    <p class="p-card">Status: ${cards.status}</p>
-    <p class="p-card">Especie: ${cards.species}</p>
-    <p class="p-card">Gênero: ${cards.gender}</p>
-    <p class="p-card">Origen: ${cards.origin.name}</p>
-    </div>`;
-  }
-
-  document.getElementById("demo").innerHTML = mostrarCard; // Imprimir os Cards no HTML
-
-})
-
-
-
-
-
-// EXIBIR TODOS OS CARDS NA TELA
-
-
-const dados = data.results; // A const dados armazena o Array com os dados puxados do objeto rickandmorty.js
-
-let mostrarCard = ""; //Condição: para cada elemento da array, adicionar um card (DIV com as propriedas LINHA 14)
-for (let cards of dados) {  // cards é a variavel [i] usado para iniciar o loop
-  mostrarCard += cards = `<div class="cards">
-  <img src="${cards.image}" class="image-card">
-  <p class="p-name-card">${cards.name}</p>
-  <p class="p-card">Status: ${cards.status}</p>
-  <p class="p-card">Especie: ${cards.species}</p>
-  <p class="p-card">Gênero: ${cards.gender}</p>
-  <p class="p-card">Origen: ${cards.origin.name}</p>
+// EXIBIR OS CARDS
+const printingAllCards = (characters) => { // A const dados armazena o array com os dados puxados do objeto rickandmorty.js
+let showingAllCards = ""; // Let que armazenará cada elemento da array que corresponder com a condição
+for (let character of characters) { // Character é a variavel índice usada para iniciar o looping
+  showingAllCards += character = `<div class="cards">
+  <img src="${character.image}" class="image-card">
+  <p class="p-name-card">${character.name}</p>
+  <p class="p-card">Status: ${character.status}</p>
+  <p class="p-card">Especie: ${character.species}</p>
+  <p class="p-card">Gênero: ${character.gender}</p>
+  <p class="p-card">Origen: ${character.origin.name}</p>
   </div>`;
 }
+document.getElementById("cardsBoard").innerHTML = showingAllCards; // Imprimir no HTML
+} 
+
+// FILTRAGENS
+// ESPÉCIES
+  const speciesFilter = document.getElementById("species"); // Const que armazena o ID do select específico
+  speciesFilter.addEventListener("change", function (event) // Ao ouvir o evento e armazenar no ID
+  {
+  const speciesCards = filter(data.results, event.target.value); // Const que armazena e busca o resultado da const filter 
+  // no data.js, filter que por sua vez armazena o objeto principal (results) do rickandmorty.js e o alvo(target) do click 
+  // no select(speciesFilter).
+  printingAllCards(speciesCards); // Armazenou o item.species do rickandmorty.js
+  });
+  printingAllCards(data.results);
 
 
-document.getElementById("demo").innerHTML = mostrarCard; // Imprimir os Cards no HTML
+// STATUS 
+  // const statusFilter = document.getElementById("status");
+  // statusFilter.addEventListener("change", function (event)
+  // {
+  // const statusCards = filter(data.results, event.target.value);
+  // printingStatusCards(statusCards);
+  // });
+  // printingStatusCards(data.results);
+  
 
