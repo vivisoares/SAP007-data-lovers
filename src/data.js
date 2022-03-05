a66ae9ba4c655522f0d11535ecad377e2c392389
Tatiane Mendonça <tatimendonz@gmail.com>
2022-03-05 20:09:39 -0300

    Filtro de Status e Specie funcionando

--- src/data.js
+++ src/data.js
@@ -1,15 +1,62 @@
+// export const filter = (characters, status) => {
+//   const results = characters.filter((item) => {
 
-    const filter = (characters, specie ) => {
-    const speciesCards = (characters.filter((item) => {
-           return item.species === specie;
-        }));
-           return speciesCards;
-}
-    // const filter = (characters, status ) => { 
-    // const statusCards = (characters.filter((item) => {
-    //     return item.status === status;
-    //  }));
-    //     return statusCards;
-    // }
+//     if (condition) { //AQUI ENTRA A CONDIÇÃO DE IF
+//       return item.characteristics === status; //Aqui esta puxando dados de Specie de cada personagem
+//     } else {
+//       return item.status === status; //Aqui esta puxando dados de Status de cada personagem
+//     }
+
+
+//   })
+//   console.log(results);
+//   return results
+// }
+
+
+
+
+
+
+
+
+// //FILTRAGEM POR ESPECIE
+
+// export const filter = (characters, characteristics) => {
+//   const results = characters.filter((item) => {
+//     return item.species === characteristics;
+//     })
+//     return results
+//   }
+
+// // export default filter
+
+
+
+
+
+
+
+
+export const filter = (characters, characteristics) => {
+  const results = characters.filter((item) => {
+    if (characteristics === item.species) {
+    return item.species === characteristics;
+    }
+    return item.status === characteristics;
+    })
+    return results
+  }
+
 
 export default filter
+
+
+
+
+// export const filter = (characters, characteristics) => {
+//   const results = characters.filter((item) => {
+//     return item.status === characteristics;
+//     })
+//     return results
+//   }
--- src/main.js
+++ src/main.js
@@ -1,42 +1,46 @@
 import data from "./data/rickandmorty/rickandmorty.js";
 import filter from './data.js';
 
-// EXIBIR OS CARDS
-const printingAllCards = (characters) => { // A const dados armazena o array com os dados puxados do objeto rickandmorty.js
-let showingAllCards = ""; // Let que armazenará cada elemento da array que corresponder com a condição
-for (let character of characters) { // Character é a variavel índice usada para iniciar o looping
-  showingAllCards += character = `<div class="cards">
-  <img src="${character.image}" class="image-card">
-  <p class="p-name-card">${character.name}</p>
-  <p class="p-card">Status: ${character.status}</p>
-  <p class="p-card">Especie: ${character.species}</p>
-  <p class="p-card">Gênero: ${character.gender}</p>
-  <p class="p-card">Origen: ${character.origin.name}</p>
-  </div>`;
-}
-document.getElementById("cardsBoard").innerHTML = showingAllCards; // Imprimir no HTML
-} 
 
-// FILTRAGENS
-// ESPÉCIES
-  const speciesFilter = document.getElementById("species"); // Const que armazena o ID do select específico
-  speciesFilter.addEventListener("change", function (event) // Ao ouvir o evento e armazenar no ID
-  {
-  const speciesCards = filter(data.results, event.target.value); // Const que armazena e busca o resultado da const filter 
-  // no data.js, filter que por sua vez armazena o objeto principal (results) do rickandmorty.js e o alvo(target) do click 
-  // no select(speciesFilter).
-  printingAllCards(speciesCards); // Armazenou o item.species do rickandmorty.js
-  });
+
+// EXIBIR OS CARDS
+const printingAllCards = (characters) => {
+  let showingCards = ""; // Let que armazenará cada elemento da array que corresponder com a condição
+  for (let character of characters) { // Character é a variavel índice usada para iniciar o looping
+    showingCards += character = `<div class="cards">
+    <img src="${character.image}" class="image-card">
+    <p class="p-name-card">${character.name}</p>
+    <p class="p-card">Status: ${character.status}</p>
+    <p class="p-card">Especie: ${character.species}</p>
+    <p class="p-card">Gênero: ${character.gender}</p>
+    <p class="p-card">Origen: ${character.origin.name}</p>
+    </div>`;
+  }
+
+  document.getElementById("cardsBoard").innerHTML = showingCards; // Imprimir no HTML
+}
+
+// FILTRAGEM POR ESPÉCIES
+
+const speciesFilter = document.getElementById("species"); // Const que armazena o ID do select específico
+speciesFilter.addEventListener("change", function (event){// Ao ouvir o evento e armazenar no ID
+
+    const speciesCards = filter(data.results, event.target.value); // Const que armazena e busca o resultado da const filter
+    printingAllCards(speciesCards); // Armazenou o item.species do rickandmorty.js
+})
   printingAllCards(data.results);
 
 
-// STATUS 
-  // const statusFilter = document.getElementById("status");
-  // statusFilter.addEventListener("change", function (event)
-  // {
-  // const statusCards = filter(data.results, event.target.value);
-  // printingStatusCards(statusCards);
-  // });
-  // printingStatusCards(data.results);
-  
+// FILTRAGEM POR STATUS
+
+  const statusFilter = document.getElementById("status");
+  statusFilter.addEventListener("change", function (event){
+
+    const results = filter(data.results, event.target.value);
+    //console.log(results);
+    printingAllCards(results); // Armazenou o item.species do rickandmorty.js
+  })
+    printingAllCards(data.results);
+
+
 
